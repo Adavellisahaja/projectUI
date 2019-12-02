f077221cf323e34b7fd3f1e155b48e2e94b477fb
Adavellisahaja <54335540+Adavellisahaja@users.noreply.github.com>
2019-12-02 10:05:48 -0600

    commit

---
+++ sample-firebase-edit/src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { filter, map, shareReplay } from 'rxjs/operators';
+import { BkLayout } from '@uibakery/kit';
+import { AngularFireDatabase} from '@angular/fire/database';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+    description = 'Angular-Fire-Demo';
+    itemValue = '';
+    items: Observable<any[]>;
+
+  defaultLayout: BkLayout = {
+    header: true,
+    headerFixed: false,
+    sidebar: true,
+    sidebarFixed: false,
+    paddings: {
+      paddingTop: 16,
+      paddingRight: 16,
+      paddingBottom: 16,
+      paddingLeft: 16
+    }
+  };
+
+  layout$: Observable<BkLayout> = this.router.events
+    .pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => {
+        let route = this.router.routerState.root;
+        while (route.firstChild) {
+          route = route.firstChild;
+        }
+        return route.snapshot.data['layout'] || this.defaultLayout;
+      }),
+      shareReplay(),
+    );
+
+  padding$: Observable<string> = this.layout$
+    .pipe(
+      map((layout: BkLayout) => {
+        const p = layout.paddings;
+        return `${p.paddingTop}px ${p.paddingRight}px ${p.paddingBottom}px ${p.paddingLeft}px`;
+      }),
+    );
+
+  constructor(private router: Router, public db: AngularFireDatabase) {
+      this.items = db.list('items').valueChanges();
+  }
+
+}
